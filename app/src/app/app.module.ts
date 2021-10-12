23535c75891ee847480574cfcaa324e8f3130cfe
Zyrotot <mathiasdutranichele@gmai.com>
2021-10-11 23:21:43 -0300

    Corrigindo PWA

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -16,10 +16,14 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
+      registrationStrategy: 'registerImmediately'
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
   ],
   declarations: [
     AppComponent,
